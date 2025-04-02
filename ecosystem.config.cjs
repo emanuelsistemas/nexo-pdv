1f299755e035da7a3370fa6941bf8095cc7a60d2
Deploy System <deploy@emasoftware.io>
2025-04-02 13:21:22 +0000

    chore: adiciona configuração PM2 para ambiente de desenvolvimento

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,36 +1,17 @@
 module.exports = {
-  apps: [
-    {
-      name: "nenxo-pdv-dev",
-      script: "pnpm",
-      args: "run dev",
-      cwd: "/root/ema-software/nenxo-pdv",
-      env: {
-        NODE_ENV: "development",
-        PORT: 5002
-      },
-      watch: false,
-      instances: 1,
-      exec_mode: "fork",
-      autorestart: true,
-      max_restarts: 10,
-      restart_delay: 5000
+  apps: [{
+    name: "nexo-pdv-dev",
+    script: "npm",
+    args: "run dev",
+    cwd: "/root/nexo-pdv",
+    env: {
+      NODE_ENV: "development",
     },
-    {
-      name: "nenxo-pdv-prod",
-      script: "pnpm",
-      args: "run preview",
-      cwd: "/root/ema-software/nenxo-pdv",
-      env: {
-        NODE_ENV: "production",
-        PORT: 5000
-      },
-      watch: false,
-      instances: 1,
-      exec_mode: "fork",
-      autorestart: true,
-      max_restarts: 10,
-      restart_delay: 5000
-    }
-  ]
+    watch: false,
+    instances: 1,
+    exec_mode: "fork",
+    autorestart: true,
+    max_restarts: 10,
+    restart_delay: 5000
+  }]
 };
