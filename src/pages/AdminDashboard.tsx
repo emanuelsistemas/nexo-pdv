4587daf43d0f924060a118dc543235e0d6b480d4
Emanuel Sistemas <emanuel.sistemas@gmail.com>
2025-05-04 14:31:36 -0300

    Corrigir warnings de atributos SVG no AdminDashboard

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -502,7 +502,7 @@ export default function AdminDashboard() {
                               className="p-1 text-blue-400 hover:text-blue-300 rounded-lg"
                               title="Editar"
                             >
-                              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                 <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
                                 <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
                               </svg>
